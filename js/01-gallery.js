4630855bec371d2fc1955ece29e173602995d6ec
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-04-24 18:28:16 +0300

    task_1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryEl.addEventListener('click', openOriginalImg);
+
+function openOriginalImg(e) {
+  const { target: image } = e;
+
+  if (image.nodeName !== 'IMG') {
+    return;
+  }
+
+  const originalImg = basicLightbox.create(`
+    <img src="${image.dataset.source}" alt="${image.alt}" >
+`);
+
+  e.preventDefault();
+
+  originalImg.show();
+
+  window.addEventListener('keydown', onEscKeyPress);
+
+  function onEscKeyPress(e) {
+    if (e.code === 'Escape') {
+      window.removeEventListener('keydown', onEscKeyPress);
+      originalImg.close();
+    }
+  }
+}
+
+function makeGalleryMarkup(gallery) {
+  return gallery
+    .map(({ description, preview, original }) => {
+      return `
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+    })
+    .join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function makeGalleryMarkup(gallery) {
+  return gallery
+    .map(({ description, preview, original }) => {
+      return `
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+    })
+    .join('');
+}
